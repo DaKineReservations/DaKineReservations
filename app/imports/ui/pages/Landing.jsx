e3a37db3a00a5e7f8cb5b86b86517210a7b28b7b
dflorenco1 <60244325+dflorenco1@users.noreply.github.com>
2021-08-12 13:44:28 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -9,7 +9,7 @@ class Landing extends React.Component {
       <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
 
         <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
+          <Image size='small' verticalAlign='left' circular src="/images/meteor-logo.png"/>
         </Grid.Column>
 
         <Grid.Column width={8}>

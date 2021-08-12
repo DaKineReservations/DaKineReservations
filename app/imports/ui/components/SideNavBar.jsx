3fbec0f2cc10b1e522b31538c0bf8962a76d08a2
christianjensenv <cjensen6@hawaii.edu>
2021-08-12 13:33:07 -1000

    99% done additional stuff can be added

--- app/imports/ui/components/SideNavBar.jsx
+++ app/imports/ui/components/SideNavBar.jsx
@@ -4,10 +4,10 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
 import { Menu, Button, Icon, IconGroup, Sidebar, Label } from 'semantic-ui-react';
-import { Roles } from 'meteor/alanning:roles';
+// import { Roles } from 'meteor/alanning:roles';
 // import 'react-pro-sidebar/dist/css/styles.css';
+import { Roles } from 'meteor/alanning:roles';
 import SignOutConfirmation from './SignOutConfirmation';
-import { UserInfos } from '../../api/userinfo/UserInfo';
 // import 'app/client/index.css';
 
 /*
@@ -39,18 +39,6 @@ class SideNavBar extends React.Component {
 
   render() {
     const { visible } = this.state;
-    // const font = "'Geo', sans-serif";
-    /*    const menuStyle = {
-      height: '100%', /!* Full-height: remove this if you want "auto" height *!/
-      width: 'auto', /!* Set the width of the sidebar *!/
-      position: 'fixed', /!* Fixed Sidebar (stay in place on scroll) *!/
-      zIndex: '1', /!* Stay on top *!/
-      top: '0', /!* Stay at the top *!/
-      left: '0',
-      /!* overflowX: 'hidden', /!* Disable horizontal scroll *!/ *!/
-      paddingTop: '20px',
-    }; */
-
     if (this.props.currentUser === '') {
       return ('');
     }
@@ -70,85 +58,65 @@ class SideNavBar extends React.Component {
           visible={visible}
           width='thin'
           color='grey'
-          style={{ height: '100vh', minHeight: '100vh' }}
+          style={{ height: '100vh', minHeight: '100vh', width: '25vw' }}
         >
           <Button fluid style={{ zIndex: 2 }} icon color='grey' disabled={false} onClick={this.handleShowClick}>
             <Icon name='bars'/>
           </Button>
-          <Menu.Item style={{ floated: 'left', width: '100%' }} id="navbar-home"
-            as={NavLink} exact to="/RestaurantHome" onClick={this.handleShowClick}>
-            <IconGroup>
-              <Icon name='home'/>
-                Home
-            </IconGroup>
-          </Menu.Item>
-          <Menu.Item style={{ floated: 'left', width: '100%' }} id="'user'"
+
+          <Menu.Item style={{ floated: 'left', width: '100%' }} key="user"
             as={NavLink} exact to="/profile" onClick={this.handleShowClick}>
             <IconGroup>
               <Icon name='user'/>
-                Profile
+              Profile
             </IconGroup>
           </Menu.Item>
-          <Menu.Item style={{ width: '100%' }} id="navbar-Prereq"
-            as={NavLink} exact to="">
+
+          <Menu.Item style={{ floated: 'left', width: '100%' }} key="Restaurants"
+            as={NavLink} exact to="/Restaurants" onClick={this.handleShowClick}>
             <IconGroup>
-              <Icon name='pencil square'/>
-                Prereq Materials
+              <Icon name='food'/>
+              Restaurants
             </IconGroup>
           </Menu.Item>
-          <Menu.Item style={{ width: '100%' }} id="navbar-Contact"
-            as={NavLink} exact to="/Contact">
+
+          <Menu.Item style={{ floated: 'left', width: '100%' }} key="CreateReservation"
+            as={NavLink} exact to="/CreateReservation" onClick={this.handleShowClick}>
             <IconGroup>
-              <Icon name='mail'/>
-                Contact Admin
+              <Icon name='edit outline'/>
+              Create Reservation
             </IconGroup>
           </Menu.Item>
+
+          <Menu.Item style={{ floated: 'left', width: '100%' }} key="MyRestaurantReservations"
+            as={NavLink} exact to="/MyRestaurantReservations" onClick={this.handleShowClick}>
+            <IconGroup>
+              <Icon name='tasks'/>
+              Restaurant Reservations
+            </IconGroup>
+          </Menu.Item>
+
           {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-            <Menu.Item style={{ width: '100%' }} id="navbar-UserList"
-              as={NavLink} exact to="/listusers" onClick={this.handleShowClick}>
+            <Menu.Item as={NavLink} activeClassName="active" exact to="/RestaurantHome" key='RestaurantHome'>
               <IconGroup>
                 <Icon name='list'/>
-                    Users
+                Restaurant Reservations
               </IconGroup>
-              <Label circular size='small' color='orange'>{this.props.count}</Label>
+            </Menu.Item>
+          ) : ''}
+
+          {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+            <Menu.Item as={NavLink} activeClassName="active" exact to="/RestaurantOptions" key='RestaurantOptions'>
+              <IconGroup>
+                <Icon name='cogs'/>
+                Restaurant Options
+              </IconGroup>
             </Menu.Item>
           ) : ''}
 
           <SignOutConfirmation id="navbar-sign-out" as={NavLink} exact to="/signout" style={{ padding: 0, margin: 0 }}/>
         </Sidebar>
       </div>
-
-    /* <Menu pointing secondary vertical style={menuStyle} attached="top" borderless inverted>
-            <Menu.Item as={NavLink} activeClassName="" exact to="/">
-              <Header inverted as='h1'>meteor-application-template</Header>
-            </Menu.Item>
-            {this.props.currentUser ? (
-              [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-                <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
-            ) : ''}
-            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
-            ) : ''}
-            <Menu.Item position="right">
-              {this.props.currentUser === '' ? (
-                <Dropdown id="login-dropdown" text="Login" pointing="top right" icon={'user'}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item id="login-dropdown-sign-in" icon="user" text="Sign In" as={NavLink} exact
-                      to="/signin"/>
-                    <Dropdown.Item id="login-dropdown-sign-up" icon="add user" text="Sign Up" as={NavLink} exact
-                      to="/signup"/>
-                  </Dropdown.Menu>
-                </Dropdown>
-              ) : (
-                <Dropdown id="navbar-current-user" text={this.props.currentUser} pointing="top right" icon={'user'}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact
-                      to="/signout"/>
-                  </Dropdown.Menu>
-                </Dropdown>
-              )}
-            </Menu.Item>
-          </Menu> */
     );
   }
 }
@@ -163,11 +131,8 @@ SideNavBar.propTypes = {
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 const NavBarContainer = withTracker(() => {
   const curr = Meteor.user() ? Meteor.user().username : '';
-  const subscription = (Roles.userIsInRole(Meteor.userId(), 'admin')) ? UserInfos.subscribeUserInfoAdmin() : UserInfos.subscribeUserInfo();
   return {
     currentUser: curr,
-    ready: subscription.ready(),
-    count: UserInfos.find({}).count(),
   };
 })(SideNavBar);
 

ce8dca4c38fe3d5a1abde31f384c750d1c653acc
dflorenco1 <60244325+dflorenco1@users.noreply.github.com>
2021-08-12 20:24:58 -1000

    Update RestOptions.jsx

--- app/imports/ui/pages/RestOptions.jsx
+++ app/imports/ui/pages/RestOptions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image, Card, CardContent, CardHeader, CardDescription, GridRow, Header, Input, Button, CardMeta } from 'semantic-ui-react';
+import { Grid, Image, Card, CardContent, CardHeader, CardDescription, GridRow, Header, Input, Button, CardMeta, GridColumn } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -11,22 +11,36 @@ class Landing extends React.Component {
         <Grid.Column width={8}>
           <Card fluid>
             <CardContent>
-              <Image circular src="/images/MaiTai.jpeg"/>
+              <Image size='medium' circular src="/images/MaiTai.jpeg"/>
+              <br></br>
+              <br></br>
 
               <Button color='blue'>Edit Logo</Button>
               <br></br>
               <br></br>
               <CardHeader>Mai Tai's Bar And Grill</CardHeader>
               <CardDescription>Casual Outdoor Ocean Side Restaurant and Bar</CardDescription>
-              <br></br>
+     
               <CardMeta>Located steps away from world famous Ala Moana Beach Park</CardMeta>
+                <br></br>
+              <Button size='tiny' color='blue'>Edit Name/Description</Button>
+              <br></br>
+              <br></br>
+              <CardDescription><strong>Hours:</strong> 12:00 PM to 1:00 AM</CardDescription>
+              <br></br>
+              <Button size='tiny' color='blue'>Edit Hours of Operation</Button>
 
-              <Button color='blue'>Edit Name/Description</Button>
-              <br></br>
-              <br></br>
-              <CardDescription>Hours: 12:00 PM to 1:00 AM</CardDescription>
-              <br></br>
-              <Button color='blue'>Edit Hours of Operation</Button>
+              <GridRow columns={2}>
+                <GridColumn>
+                  <Button>View/Change Restaurant Menu</Button>
+                </GridColumn>
+
+                <GridColumn>
+                  <Button>Update Seating Allocation</Button>
+                </GridColumn>
+                
+              </GridRow>
+
 
             
 

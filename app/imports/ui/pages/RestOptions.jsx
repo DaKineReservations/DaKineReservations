4a06b8b08ffcef3f2e41365cb3da976035a7f283
dflorenco1 <60244325+dflorenco1@users.noreply.github.com>
2021-08-12 15:29:32 -1000

    Rest Options additions

--- app/imports/ui/pages/RestOptions.jsx
+++ app/imports/ui/pages/RestOptions.jsx
@@ -1,39 +1,36 @@
 import React from 'react';
-import { Grid, Image, Card, CardContent, CardHeader, CardDescription, GridRow, Header, Input, Button } from 'semantic-ui-react';
+import { Grid, Image, Card, CardContent, CardHeader, CardDescription, GridRow, Header, Input, Button, CardMeta } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
       
-      <Grid id='landing-page' verticalAlign='left' textAlign='center' container>
+      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
 
         <Grid.Column width={8}>
           <Card fluid>
             <CardContent>
-              <Image circular src="/images/dandakine.jpg"/>
-              <Button>Upload Image</Button>
+              <Image circular src="/images/MaiTai.jpeg"/>
+
+              <Button color='blue'>Edit Logo</Button>
+              <br></br>
+              <br></br>
+              <CardHeader>Mai Tai's Bar And Grill</CardHeader>
+              <CardDescription>Casual Outdoor Ocean Side Restaurant and Bar</CardDescription>
+              <br></br>
+              <CardMeta>Located steps away from world famous Ala Moana Beach Park</CardMeta>
+
+              <Button color='blue'>Edit Name/Description</Button>
+              <br></br>
+              <br></br>
+              <CardDescription>Hours: 12:00 PM to 1:00 AM</CardDescription>
+              <br></br>
+              <Button color='blue'>Edit Hours of Operation</Button>
+
+            
 
 
-              <GridRow>
-                <Header textAlign='left'>Name</Header>
-                <Input fluid verticalAlign='left' foucs placeholder='name'/> <Button>Submit</Button>
-
-              </GridRow>
-             
-              <GridRow>
-                <Header textAlign='left'>Number</Header>
-                <Input fluid verticalAlign='left' foucs placeholder='(808)555-5555'/> <Button>Submit</Button>
-              </GridRow>
-             
-              <GridRow>
-                <Header textAlign='left'>Change Password</Header>
-                <Input fluid verticalAlign='left' foucs placeholder='current password'/>
-                <br></br>
-                <Input fluid verticalAlign='left' foucs placeholder='new password'/>
-                <br></br>
-                <Input fluid verticalAlign='left' foucs placeholder='confirm password'/> <Button>Submit</Button>
-              </GridRow>
 
             </CardContent>
           </Card>

c6178fad8f9a0838b92ec832f3ad824c7e59d0b9
vjodar <vjodar@hawaii.edu>
2021-08-13 00:40:13 -1000

    Finished CreateReservation page

--- app/imports/ui/pages/CreateReservation.jsx
+++ app/imports/ui/pages/CreateReservation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Container, Segment } from 'semantic-ui-react';
-import { AutoForm, AutoField, TextField, SelectField, ErrorsField, SubmitField } from 'uniforms-semantic';
+import { Container, Segment, Progress, Grid, Header, Image } from 'semantic-ui-react';
+import { AutoForm, AutoField, LongTextField, SelectField, ErrorsField, SubmitField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
@@ -39,18 +39,43 @@ const CreateReservationSchema = new SimpleSchema2Bridge(
 class CreateReservation extends React.Component {
   render() {
     return (
-      <Container id='createReservation' container>
+      <Container id='createReservation'>
+
+        <Progress percent={66} success>
+            <Grid columns='equal'>
+                <Grid.Column>Browse Restaurants</Grid.Column>
+                <Grid.Column>Create Reservation</Grid.Column>
+                <Grid.Column>Confirm Reservation</Grid.Column>
+            </Grid>
+        </Progress>
+        
+        <Header as="h1" textAlign="center">Make A Reservation</Header>
+        <Segment>
+            <Grid centered divided padded verticalAlign='middle'>
+                 <Grid.Column width={4}>
+                     <Image size='medium' src='https://image.shutterstock.com/image-vector/wine-glass-bottle-logo-design-600w-626927420.jpg'/>
+                 </Grid.Column>
+                 <Grid.Column width={10}>
+                     <Segment textAlign="center" padded="very">
+                         <Header as='h2' textAlign='center'>Mai Tai's</Header>
+                         <Header as='h3' textAlign='center'>Hours: 12:00 PM - 1:00 AM</Header>
+                     </Segment>
+                  </Grid.Column>
+              </Grid>
+        </Segment>
+        
         <AutoForm schema={CreateReservationSchema} 
         onSubmit={(model) => swal('Reservation Created For:', `${(model.startTime)}`, 'success')}>
             <Segment>
               <AutoField name='startTime'/>
               <AutoField name='endTime'/>
               <SelectField name='seatingPreference'/>
-              <TextField name='specialRequests'/>
+              <LongTextField name='specialRequests'/>
               <SubmitField value='Submit'/>
               <ErrorsField/>
             </Segment>
           </AutoForm>
+
       </Container>
     );
   }
